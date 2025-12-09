23afb487733fbddbd88c341d8439f3cf8d3a84dc
dev-abraao <abraao_andrade_sousa@hotmail.com>
2025-12-08 23:18:31 -0300

    added comments

--- app/Controller/ServiceProvidersController.php
+++ app/Controller/ServiceProvidersController.php
@@ -70,8 +70,9 @@ class ServiceProvidersController extends AppController {
                         $this->Flash->notification('A foto é muito grande. O tamanho máximo permitido é 5MB.', array('params' => array('class' => 'error')));
                         return $this->redirect(array('action' => 'create'));
                     }
-
+                    // gera nome para o arquivo da foto
                     $filename = uniqid('photo_') . '.' . $extension;
+                    // diretório de upload
                     $uploadDir = WWW_ROOT . 'img' . DS . 'uploads' . DS;
                     
                     if (!is_dir($uploadDir)) {
@@ -134,21 +135,22 @@ class ServiceProvidersController extends AppController {
             // Upload de Foto
             if (!empty($this->request->data['ServiceProvider']['photo']['name'])) {
                 $photo = $this->request->data['ServiceProvider']['photo'];
-
+                // verificacao de tipo de arquivo
                 $extension = pathinfo($photo['name'], PATHINFO_EXTENSION);
                 if (!in_array(strtolower($extension), array('jpg', 'jpeg', 'png'))) {
                     $this->Flash->notification('Por favor, envie um arquivo de foto(JPG, JPEG, PNG) válido.', array('params' => array('class' => 'error')));
                     return $this->redirect(array('action' => 'edit', $id));
                 }
-
+                // verificacao de tamanho maximo 5MB
                 if ($photo['error'] === UPLOAD_ERR_OK) {
                     $photosize = filesize($photo['tmp_name']);
                     if ($photosize > 5 * 1024 * 1024) {
                         $this->Flash->notification('A foto é muito grande. O tamanho máximo permitido é 5MB.', array('params' => array('class' => 'error')));
                         return $this->redirect(array('action' => 'edit', $id));
                     }
-
+                    // gera nome para o arquivo da foto
                     $filename = uniqid('photo_') . '.' . $extension;
+                    // diretório de upload
                     $uploadDir = WWW_ROOT . 'img' . DS . 'uploads' . DS;
                     
                     if (!is_dir($uploadDir)) {
